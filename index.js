e605bd0e74ca9d3653ea1bb8b34d758799843182
Fahwaaz Damonse <fahwaaz.damonse@clickatell.com>
2022-06-25 18:28:04 +0200

    changed stuff

--- index.js
+++ index.js
@@ -1,54 +1,106 @@
-const submitForm = document.getElementById("submit");
-submitForm.addEventListener("click", () => {
-  alert("Form successfully submitted");
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", () => {
+  alert("The form has been submitted");
 });
 
-const description = document.getElementById("description").value,
-  addTask = document.getElementById("addTask"),
-  saveTask = document.getElementById("saveToDo"),
-  itemContainer = document.getElementById("taskBorder"),
+const itemContainer = document.getElementById("taskBorder"),
   saveIndex = document.getElementById("saveIndex");
 
-let items = [];
-
-addTask.addEventListener("click", (e) => {
-  let toDo = localStorage.getItem("toDo");
-  if (toDo === null) {
-    items = [];
-  } else {
-    items = JSON.parse(toDo);
-  }
-
-  items.push(description);
-  description.value = "";
-  localStorage.setItem("toDO", JSON.stringify(items));
-  showItems(items);
-});
+let items = [
+  {
+    id: 1,
+    description: "add task",
+    dateCreated: Date(),
+    completed: false,
+  },
+];
 
 function showItems(items) {
-  let toDo = localStorage.getItem("toDo");
-  if (toDo === null) {
-    items = [];
-  } else {
-    items = JSON.parse(toDo);
-  }
-
   itemContainer.innerHTML = "";
-  items.forEach((item, ind) => {
+
+  items.forEach((item) => {
     itemContainer.innerHTML += `
-        <div
+        <div 
           id="itemContainer"
-          class="d-flex justify-content-between align-items-center m-4 border w-75"
+          class="d-flex justify-content-between align-items-center m-4 border w-75 col-md-12"
         >
           <input type="checkbox" name="completed" id="completed" class="ms-3" />
           <h4 id="description">${item.description}</h4>
           <div>
-            <i class="fa-solid fa-pencil" onclick='edit(${ind})'></i>
-            <i class="fa-solid fa-ban me-3" onclick='deleteTask(${ind})'></i>
+            <button onclick = 'editTask(id)'>
+            <i class="fa-solid fa-pencil p-1"></i>
+            </button>
+            <button  onclick='deleteTask(id)'>
+            <i class="fa-solid fa-ban p-1"></i>
+            </button>
           </div>
         </div>
 
     `;
+    console.log(`
+    Task with ID of ${item.id} has been created on ${item.dateCreated}`);
   });
 }
-console.log(showItems(items));
+showItems(items);
+
+const addTask = (e) => {
+  const newTask = {
+    id: items.length + 1,
+    description: document.getElementById("description").value,
+    dateCreated: Date(),
+    completed: false,
+  };
+  items.push(newTask);
+  showItems(items);
+  console.log(items);
+};
+
+function deleteTask(id) {
+  items = items.filter((item) => item.id !== id);
+  showItems(items);
+}
+
+function editTask(id) {
+  const editedDescription = prompt(
+    "What would you like to change your task to?"
+  );
+  const item = items.find((item) => item.id === id);
+  items.description = editedDescription.value;
+  showItems(items);
+}
+
+const filteredTasks = (e) => {
+  const Filter = e.target.value;
+
+  const filterByName = items.sort((a, b) => {
+    if (a.description.toLowerCase() < b.description.toLowerCase()) {
+      return -1;
+    }
+    if (a.description.toLowerCase() > b.description.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const filterByDate = items.sort((a, b) => {
+    if (a.dateCreated < b.dateCreated) {
+      return -1;
+    }
+    if (a.dateCreated > b.dateCreated) {
+      return 1;
+    }
+    return 0;
+  });
+
+  if (Filter === "none") {
+    return showItems(items);
+  } else if (Filter === "nameAcsending") {
+    return showItems(filterByName);
+  } else if (Filter === "nameDescsending") {
+    return showItems(filterByName.reverse);
+  } else if (Filter === "dateAcsending") {
+    return showItems(filterByDate);
+  } else if (Filter === "dateDescsending") {
+    return showItems(filterByDate.reverse);
+  }
+};
